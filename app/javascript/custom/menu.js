6cabdd0c13bb8730265e90289501e749ca37e6d0
macA360 <mackenzie.e.cox@bath.edu>
2024-01-23 16:14:32 +0000

    small bugs

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,17 +1,21 @@
 // Menu manipulation
 
-// Adds a toggle listener.
+// Adds a toggle listener if the element exists.
 function addToggleListener(selected_id, menu_id, toggle_class) {
     let selected_element = document.querySelector(`#${selected_id}`);
-    selected_element.addEventListener("click", function(event) {
-      event.preventDefault();
-      let menu = document.querySelector(`#${menu_id}`)
-      menu.classList.toggle(toggle_class);
-    });
-  }
-  
-  // Add toggle listeners to listen for clicks.
-  document.addEventListener("turbo:load", function() {
+    if (selected_element) {
+        selected_element.addEventListener("click", function(event) {
+            event.preventDefault();
+            let menu = document.querySelector(`#${menu_id}`);
+            if (menu) {
+                menu.classList.toggle(toggle_class);
+            }
+        });
+    }
+}
+
+// Add toggle listeners to listen for clicks.
+document.addEventListener("turbo:load", function() {
     addToggleListener("hamburger", "navbar-menu",   "collapse");
     addToggleListener("account",   "dropdown-menu", "active");
-  });
+});
